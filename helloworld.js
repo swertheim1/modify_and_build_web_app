44edfdfd09315a6db442060dde10a2cced0a30e5
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-01-28 09:35:32 -0600

    update port to 3333

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Welcome to the World of DevOps!\n')
-}).listen(3000)
+}).listen(3333)
 
 // Console will print the message
 console.log('Server running')
