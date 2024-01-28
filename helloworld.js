a1114249ab63496066c3a517b1113b5332a9d48b
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-01-28 09:42:30 -0600

    added ; to end of each command

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,15 @@
-var http = require("http")
+var http = require("http");
 
 http.createServer(function (request, response) {
 
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'})
+   response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
    response.end('Welcome to the World of DevOps!\n')
-}).listen(3333)
+}).listen(3333);
 
 // Console will print the message
-console.log('Server running')
+console.log('Server running');
